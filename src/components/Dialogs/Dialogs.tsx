a3f80e7c8b55d345b4cfa5328cf046c4846a0d6e
e1ing <elvira.kisling@gmail.com>
2021-05-10 21:41:21 +0300

    video-40

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -41,7 +41,7 @@ const Dialogs: React.FC<PropsType> = ({dialogsPage, dispatch}) => {
                 <div>{messagesElements}/<div>
                     <div>
                         <div><textarea value={newMessageBody}
-                                       onCjange={onNewMessageChange}
+                                       onChange={onNewMessageChange}
                                        placeholder="Enter message"> </textarea></div>
                         <div><button onClick={onSendMessageClick}> Send </button></div>
                     </div>
