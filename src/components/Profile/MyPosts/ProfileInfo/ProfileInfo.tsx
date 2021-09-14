bab4f0fa729a4df2fc760f77facdf4ac3bfab0cf
e1ing <elvira.kisling@gmail.com>
2021-09-14 22:10:07 +0300

    video-97 start 11:44

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -16,15 +16,14 @@ type ProfileInfoPropsType = {
     savePhoto: (file: File) => void
 }
 
-const ProfileInfo: FC<ProfileInfoPropsType> = ({profile,status, updateStatus, isOwner, savePhoto}) => {
-    if(!profile){
-    return <Preloader/>
-}
+const ProfileInfo: FC<ProfileInfoPropsType> = ({profile, status, updateStatus, isOwner, savePhoto}) => {
+    if (!profile) {
+        return <Preloader/>
+    }
 
 
     const onMainPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.target.files.length)
-        {
+        if (e.target.files.length) {
             savePhoto(e.target.files[0])
         }
     }
@@ -34,11 +33,38 @@ const ProfileInfo: FC<ProfileInfoPropsType> = ({profile,status, updateStatus, is
                 src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
         </div>
         <div className="classes.descriptionBlock">
-            <img src={profile.photos.large||userPhoto} className={s.mainPhoto}></img>
-            {isOwner && <input type = {"file"} onChange = {onMainPhotoSelected} />}
+            <img src={profile.photos.large || userPhoto} className={s.mainPhoto}></img>
+            {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
+
+            <div>
+                <div>
+                    <b> Full name </b>{profile.lookingForAJob ? "yes" : "no"}
+                </div>
+                <div>
+                   <b> Looking for a job: </b>{profile.lookingForAJob ? "yes" : "no"}
+                </div>
+
+                {profile.lookingForAJob &&
+                <div>
+                <b>My professional skills:</b> {profile.lookingForAJobDescription}
+                </div>
+                    }
+
+                <div>
+                    <b> About me:</b> {profile.aboutMe ? "yes" : "no"}
+                </div>
+                <div>
+                    <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {})}
+                </div>
+            </div>
+
             <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
     </div>
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div><b>{contactTitle}</b>: {contactValue}</div>
 }
 export default ProfileInfo;
 
